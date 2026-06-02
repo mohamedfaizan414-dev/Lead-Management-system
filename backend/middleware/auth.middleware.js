6447d3631177185f15aeb8a00d2397fc21898b90
Mohamed Faizan <mohamedfaizan414@gmail.com>
2026-06-02 14:21:40 +0530

    debugging

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,23 +1,27 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken")
+const userModel = require("../model/user.model")
 
-function authMiddleware(req, res, next) {
+module.exports = async function auth(req, res, next) {
+  try {
+   
     const authHeader = req.headers.authorization
 
-
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ message: "Unauthorized: Missing or malformed token" })
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Unauthorized" })
     }
 
+    const token = authHeader.split(" ")[1]
 
-    const token = authHeader.split(' ')[1]
-    
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        req.userId = decoded.id
-        next()
-    } catch {
-        res.status(401).json({ message: "Invalid or expired token" })
-    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    const user = await userModel.findById(decoded.id)
+
+    if (!user) return res.status(401).json({ message: "Invalid token" })
+
+    req.user = user
+    next()
+  } catch (err) {
+    console.log(err)
+    res.status(401).json({ message: "hii",err })
+  }
 }
-
-module.exports = authMiddleware
